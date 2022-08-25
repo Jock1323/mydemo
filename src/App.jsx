06557d5ca649759136e5ec21bfbf68a5a25d5847
Jock1323 <mirsidiq2002@gmail.com>
2022-08-25 14:31:16 +0500

    asdasdasdsa

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=apple&from=2022-08-24&to=2022-08-24&sortBy=popularity&apiKey=cd5989874f8649ebb2dca88790fc68e4"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data.articles));
+  }, []);
+  console.log(data);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {data.map((item, index) => (
+        <p key={index}>{item.title}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
